0c5c8e6b9ca6f3970353a97f71cd6cdb5ffe9656
James Clifford <engineer@jclifford.ie>
2018-05-13 23:10:23 +0100

    tab filename

--- app/overrides/add_comment_to_admin_shipments.rb
+++ app/overrides/add_comment_to_admin_shipments.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/orders/_shipment",
                     :name => "comments_for_order_shipment",
                     :insert_after => "[data-hook='admin_shipment_form']",
-                    :partial => "spree/admin/shipments/tab",
+                    :partial => "spree/admin/shipments/list_tab",
                     :disabled => false)
