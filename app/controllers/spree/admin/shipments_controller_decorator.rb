43afbd54417ab7fb0c6e56feb01f077ce265b920
99cm <zyizyao@china-guide.com>
2018-06-12 18:31:28 -0400

    fix rubocop errors

--- app/controllers/spree/admin/shipments_controller_decorator.rb
+++ app/controllers/spree/admin/shipments_controller_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Admin::ShipmentsController.class_eval do
   def comments
     @order = @shipment.order
-    @comment_types = Spree::CommentType.where(:applies_to => "Shipment")
+    @comment_types = Spree::CommentType.where(applies_to: 'Shipment')
   end
 end
