ef8437f6375fd86468aba02276e19eceeb057644
99cm <zyizyao@china-guide.com>
2018-06-12 21:37:07 -0400

    Add user order comments

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,3 +1,19 @@
 Spree::Order.class_eval do
   acts_as_commentable
+
+  def comment
+    comments.first.comment
+  end
+
+  def comment=(text)
+    user_id ||= 1 # Set the admin as owner when no user is avaible.
+
+    if comments.empty?
+      comments.create(comment: text, user_id: user_id)
+    else
+      comment = comments.first
+      comment.comment = text
+      comment.save
+    end
+  end
 end
