63b74c82265e5f82f5979cc65ebce3a2e8735a76
99cm <zyizyao@china-guide.com>
2018-06-13 12:30:34 -0400

    Update comment_mailer.rb

--- app/mailers/spree/comment_mailer.rb
+++ app/mailers/spree/comment_mailer.rb
@@ -1,11 +1,11 @@
 module Spree
   class CommentMailer < Spree::BaseMailer
-    default from: Spree::Store.current.mail_from_address
+    default from: Spree::current_store.mail_from_address
 
     def comment(comment, shipment, others)
     	@comment = comment
       @shipment = shipment
-      mail to: others.pluck(:email), subject: 'New comment from '+comment.user.name+' for '+shipment.number, name: Spree::current_store.name
+      mail(to: @user.email, subject: 'New comment from '+comment.user.name+' for '+shipment.number, name: Spree::current_store.name)
     end
   end
 end
