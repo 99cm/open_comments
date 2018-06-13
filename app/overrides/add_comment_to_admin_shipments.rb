1f77c425af0a63a008840bd03531ca55ef75e032
99cm <zyizyao@china-guide.com>
2018-06-12 21:23:56 -0400

    fix rubocop errors

--- app/overrides/add_comment_to_admin_shipments.rb
+++ app/overrides/add_comment_to_admin_shipments.rb
@@ -1,5 +1,7 @@
-Deface::Override.new(:virtual_path => "spree/admin/orders/_shipment",
-                    :name => "comments_for_order_shipment",
-                    :insert_after => "[data-hook='admin_shipment_form']",
-                    :text => '<%= render "spree/admin/shared/comments", {:commentable => shipment, :comments_for => "Borrow ##{shipment.number}"} %>',
-                    :disabled => false)
+Deface::Override.new(
+  virtual_path: 'spree/admin/orders/_shipment',
+  name: 'comments_for_order_shipment',
+  insert_after: '[data-hook='admin_shipment_form']',
+  text: '<%= render 'spree/admin/shared/comments', {commentable: shipment, comments_for: 'Borrow ##{shipment.number}'} %>',
+  disabled: false
+  )
