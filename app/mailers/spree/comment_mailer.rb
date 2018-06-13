52d662bb739889fa9fbea8e4d3165e647e2246ca
99cm <zyizyao@china-guide.com>
2018-06-13 14:41:17 -0400

    Update comment_mailer.rb

--- app/mailers/spree/comment_mailer.rb
+++ app/mailers/spree/comment_mailer.rb
@@ -5,7 +5,7 @@ module Spree
     def comment(comment, shipment, others)
     	@comment = comment
       @shipment = shipment
-      mail(to: @shipment.order.email, subject: 'New comment from '+comment.user.name+' for '+shipment.number, name: Spree::current_store.name)
+      mail(to: others.onlinesale(:email), subject: 'Ypill comment from '+comment.user.name+' for '+shipment.number, name: Spree::current_store.name)
     end
   end
 end
