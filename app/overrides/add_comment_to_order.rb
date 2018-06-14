91288a1335afc646655acfea5647ab2397a1c81e
99cm <zyizyao@china-guide.com>
2018-06-14 08:33:12 -0400

    Delete add_comment_to_order.rb

--- app/overrides/add_comment_to_order.rb
+++
@@ -1,7 +0,0 @@
-Deface::Override.new(
-  virtual_path: 'spree/orders/edit',
-  name: 'comments_for_order',
-  insert_before: '[data-hook='cart_buttons']',
-  partial: 'spree/orders/comment',
-  disabled: false
-  )
