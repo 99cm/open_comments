ee3d66ec0c85840a90acb0f4825837d08deff24e
99cm <zyizyao@china-guide.com>
2018-06-23 15:06:46 -0400

    Update add_comment_to_admin_shipments.rb

--- app/overrides/add_comment_to_admin_shipments.rb
+++ app/overrides/add_comment_to_admin_shipments.rb
@@ -2,6 +2,6 @@ Deface::Override.new(
   virtual_path: 'spree/admin/orders/_shipment',
   name: 'comments_for_order_shipment',
   insert_after: "[data-hook = 'admin_shipment_form']",
-  text: '<%= render 'spree/admin/shared/comments', { commentable: shipment, comments_for: 'Borrow ##{shipment.number}' } %>',
+  text: '<%= render ('spree/admin/shared/comments'), { commentable: shipment, comments_for: '##{shipment.number}' } %>',
   disabled: false
   )
