cb1a3497a5f7d9ab9fb8f232f1d6ed05b344d314
99cm <zyizyao@china-guide.com>
2018-06-23 14:27:08 -0400

    Update add_comment_configuration.rb

--- app/overrides/add_comment_configuration.rb
+++ app/overrides/add_comment_configuration.rb
@@ -1,7 +1,6 @@
 Deface::Override.new(
   virtual_path: 'spree/admin/shared/sub_menu/_configuration',
   name: 'converted_admin_configurations_menu_468573024',
-  insert_bottom: '[data-hook='admin_configurations_sidebar_menu']',
+  insert_bottom: '(data-hook='admin_configurations_sidebar_menu')',
   text: '<%= configurations_sidebar_menu_item t(:comment_types), admin_comment_types_path %>',
-  disabled: false
-  )
+  disabled: false)
