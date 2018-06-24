7e279a0b4235278f3b1868520606492855f7e405
99cm <zyizyao@china-guide.com>
2018-06-24 12:23:03 -0400

    fix bootsnap load cache path errors

--- app/controllers/spree/admin/shipments_controller_decorator.rb
+++ app/controllers/spree/admin/shipments_controller_decorator.rb
@@ -1,4 +1,4 @@
-Spree::Admin::ShipmentsController.class_eval do
+Spree::Api::V1::ShipmentsController.class_eval do
   def comments
     @order = @shipment.order
     @comment_types = Spree::CommentType.where(applies_to: 'Shipment')
