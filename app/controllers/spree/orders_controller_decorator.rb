e3f463ac9eeeedb8a5bcc229f10dc7dd50fe56e9
99cm <zyizyao@china-guide.com>
2018-06-24 17:24:41 -0400

    Update orders_controller_decorator.rb

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -4,5 +4,4 @@ Spree::OrdersController.class_eval do
     edit_without_comments
     @order.comments.build if @order.comments.empty? # At the moment the first user is set to be the creator
   end
-  alias_method_chain :edit, :comments
 end
